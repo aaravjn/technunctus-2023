bc0242e9c7fc9307ed6723c9e6dfd4345d0738a1
Aarav Jain <ajaarav14@gmail.com>
2023-02-02 13:05:52 +0530

    added fonts

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,14 +1,22 @@
-import "../css/navbar.css"
-
+import "../../css/navbar.css"
+import logo from "../../assets/logo.png"
+import { Box } from '@mui/material'
 
 export default function Navbar() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg">
+            <nav className="navbar fixed navbar-expand-lg">
                 <div className="navbar-cont container-fluid">
-                    <div className="brand text">
-                        <img src={require("../assets/logo.png")} height="37" width={42}/>
-                        <span>Technunctus</span>
+                    <div className="link">
+                        <Box
+                            component='img'
+                            sx={{height: 55, width: 55}}
+                            alt="logo"
+                            src={logo}
+                        />
+                        <span className="brand">
+                            Technunctus
+                        </span>
                     </div>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -16,19 +24,19 @@ export default function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mb-2 mb-lg-0 links">
                         <li className="nav-item">
-                            <a className="nav-link text" aria-current="page" href="/">Home</a>
+                            <a className="nav-link link" href="/">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text" href="/">Link</a>
+                            <a className="nav-link link" href="/">Link</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text" href="/">About</a>
+                            <a className="nav-link link" href="/">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text" href="/">Sponsors</a>
+                            <a className="nav-link link" href="/">Sponsors</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text" href="/">Events</a>
+                            <a className="nav-link link" href="/">Events</a>
                         </li>
                     </ul>
                     
