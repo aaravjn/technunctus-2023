789052a052f92e4166cb72a5f2b75694c1363a12
Aarav <ajaaravbvg@gmail.com>
2023-03-09 16:13:42 +0530

    improved footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -8,90 +8,155 @@ export default function Footer() {
     return (
         <>
 
-            <footer className="footer">
-                <div className="footer-container">
-                    <div className="footer-col">
-                        <ul>
-                            <li className='footer-links'>
-                                <Link to="/" >
-                                    <motion.a className="hoverLink m-2 ">Home</motion.a>
-                                </Link>
-                            </li>
-                            <li className='footer-links'>
-                                <Link to="/a" >
-                                    <motion.a className="hoverLink m-2 ">About Us</motion.a>
-                                </Link>
-                            </li>
-                            <li className='footer-links'>
-                                <Link to="/e" >
-                                    <motion.a className="hoverLink m-2 ">Events</motion.a>
-                                </Link>
-                            </li>
-                            <li className='footer-links'>
-                                <Link to="/s" >
-                                    <motion.a className="hoverLink m-2 ">Sponsors</motion.a>
-                                </Link>
-                            </li>
-                            <li className='footer-links'>
-                                <Link to="/h" >
-                                    <motion.a className="hoverLink m-2 ">Highlights</motion.a>
-                                </Link>
-                            </li>
-                            <li className='footer-links'>
-                                <Link to="/t/night" >
-                                    <motion.a className="hoverLink m-2 ">TechNights</motion.a>
-                                </Link>
-                            </li>
-                            <li className='footer-links'>
-                                <Link to="/policy" >
-                                    <motion.a className="hoverLink m-2 ">Privacy<br/> Policy</motion.a>
-                                </Link>
-                            </li>
-                            <li className='footer-links'>
-                                <Link to="/terms" >
-                                    <motion.a className="hoverLink m-2 ">
-                                        Terms and<br/>Conditions
-                                    </motion.a>
-                                </Link>
-                            </li>
-                            <li className='footer-links'>
-                                <Link to="/cancellation" >
-                                    <motion.a className="hoverLink m-2 ">
-                                        Cancellation<br/>policy
-                                    </motion.a>
-                                </Link>
-                            </li>
-                        </ul>
-                        <ul>
-                            <li className="m-2 italic text-sm">
-                                <h4>Developers&apos; github profile</h4>
-                            </li>
-                            <li>
-                                <Link to="https://github.com/aaravjn">
-                                    <motion.a className="hoverLink m-2 aarav">Aarav Jain</motion.a>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="https://github.com/kapiswayprakhar15" >
-                                    <motion.a className="hoverLink m-2 ">Prakhar Kapisway</motion.a>
-                                </Link>
-                            </li>{" "}
-                            <li>
-                                <Link to="https://github.com/yshubham8419" >
-                                    <motion.a className="hoverLink m-2 ">Shubham Yadav</motion.a>
-                                </Link>
-                            </li>
-                        </ul>
-                        
-                    </div>
-                    <div className="footer-col-2">
+                <footer className="footer row">
+                    <ul className='col-3 footer-col-1' style={{marginLeft:"4%"}}>
+                        <li>
+                            <Link to="/" >
+                                <motion.a className='f-link'
+                                    whileHover={{
+                                        paddingLeft:"12px",
+                                        transition:{duration:0.2}
+                                    }}
+                                >Home</motion.a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/a" >
+                                <motion.a className='f-link'
+                                    whileHover={{
+                                        paddingLeft:"12px",
+                                        transition:{duration:0.2}
+                                    }}
+                                >About Us</motion.a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/e" >
+                                <motion.a className='f-link'
+                                    whileHover={{
+                                        paddingLeft:"12px",
+                                        transition:{duration:0.2}
+                                    }}
+                                >Events</motion.a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/s">
+                                <motion.a className='f-link'
+                                    whileHover={{
+                                        paddingLeft:"12px",
+                                        transition:{duration:0.2}
+                                    }}
+                                >Sponsors</motion.a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/h">
+                                <motion.a className='f-link'
+                                    whileHover={{
+                                        paddingLeft:"12px",
+                                        transition:{duration:0.2}
+                                    }}
+                                >Highlights</motion.a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/t/night">
+                                <motion.a className='f-link'
+                                    whileHover={{
+                                        paddingLeft:"12px",
+                                        transition:{duration:0.2}
+                                    }}
+                                >TechNights</motion.a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/policy" >
+                                <motion.a className='f-link'
+                                    whileHover={{
+                                        paddingLeft:"12px",
+                                        transition:{duration:0.2}
+                                    }}
+                                >Privacy Policy</motion.a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/terms" >
+                                <motion.a className='f-link'
+                                    whileHover={{
+                                        paddingLeft:"12px",
+                                        transition:{duration:0.2}
+                                    }}
+                                >
+                                    Terms and Conditions
+                                </motion.a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/cancellation" >
+                                <motion.a className='f-link'
+                                    whileHover={{
+                                        paddingLeft:"12px",
+                                        transition:{duration:0.2}
+                                    }}
+                                >
+                                    Cancellation policy
+                                </motion.a>
+                            </Link>
+                        </li>
+                    </ul>
+                    <ul className='col footer-col-2'>
+                        <li>
+                            <h4 className='profiles-heading'>Developers' github profile</h4>
+                            <hr style={{
+                                border:"none",
+                                height:"3px",
+                                width:"30%",
+                                backgroundColor:"#e91e63",
+                                opacity:"1"
+                            }}/>
+                        </li>
+                        <li>
+                            <Link to="https://github.com/aaravjn">
+                                <motion.a className="aarav f-link"
+                                    whileHover={{
+                                        paddingLeft:"12px",
+                                        transition:{duration:0.2}
+                                    }}
+                                >Aarav Jain</motion.a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="https://github.com/kapiswayprakhar15" >
+                                <motion.a className='f-link'
+                                    whileHover={{
+                                        paddingLeft:"12px",
+                                        transition:{duration:0.2}
+                                    }}
+                                >Prakhar Kapisway</motion.a>
+                            </Link>
+                        </li>{" "}
+                        <li>
+                            <Link to="https://github.com/yshubham8419" >
+                                <motion.a className='f-link'
+                                    whileHover={{
+                                        paddingLeft:"12px",
+                                        transition:{duration:0.2}
+                                    }}
+                                >Shubham Yadav</motion.a>
+                            </Link>
+                        </li>
+                    </ul>
+                    <div className="col" style={{
+                        paddingLeft:"10%",
+                    }}>
                         <img
                             src={require("../../assets/logo.png")}
                             alt="Footer Head Logo"
-                            className='technunctuslogo'
+                            style={{height:"50%", width:"45%"}}
                         />
-                        <b className="mt-4 text-3xl font-bold block">Technunctus</b>
-                        <b>Indian Institute of Technology, Jammu</b>
+                        <p style={{fontWeight:"bold"}}>Technunctus</p>
+                        <p>Indian Institute of Technology, Jammu</p>
                         <p className="text-sm">
                             Jagti, NH-44 , PO Nagrota
                             <br />
@@ -99,56 +164,43 @@ export default function Footer() {
                             <br />
                             Tel.: 0191-2570381{" "}
                         </p>
-                        <ul className="footer-link">
+                        <ul className='social-links' style={{textAlign:"left", paddingLeft:"0px"}}>
                             <li>
-                                <Link to="https://www.iitjammu.ac.in/" >
-                                    
-                                    <img src={require("../../assets/icons/web-logo.png")} alt="iitjammuicon" className='iitjammuicon' style={{height:"35px", width:"35px"}}/>
+                                <Link to="https://www.iitjammu.ac.in/" >   
+                                    <img src={require("../../assets/icons/web-logo.png")} alt="iitjammuicon" style={{height:"35px", width:"35px"}}/>
                                 </Link>
                             </li>
                             <li>
                                 <Link
                                     to="https://www.linkedin.com/school/iitjammu/?originalSubdomain=in"
-
                                 >
-                                    {/* <motion.a className="hover:gradientText"> */}
                                     <img src={require("../../assets/icons/linkedin.png")} alt="linkedinicon"/>
-                                    {/* </motion.a> */}
                                 </Link>
                             </li>
                             <li>
                                 <Link
                                     to="https://twitter.com/IITJammu?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
                                 >
-                                    {/* <motion.a className="hover:gradientText"> */}
                                     <img src={require("../../assets/icons/twitter.png")} alt="twittericon" />
-                                    {/* </motion.a> */}
                                 </Link>
                             </li>{" "}
                             <li>
                                 <Link
                                     to="https://www.facebook.com/IITJammuOfficial/"
-
                                 >
-                                    {/* <motion.a className="hover:gradientText"> */}
                                     <img src={require("../../assets/icons/facebook.png")} alt="facebookicon" />
-                                    {/* </motion.a> */}
                                 </Link>
                             </li>{" "}
                             <li>
                                 <Link
                                     to="https://www.instagram.com/technunctus/"
-
                                 >
-                                    {/* <motion.a className="hover:gradientText"> */}
                                     <img src={require("../../assets/icons/insta.png")} alt="instaicon" />
-                                    {/* </motion.a> */}
                                 </Link>
                             </li>
                         </ul>
                     </div>
-                </div>
-            </footer >
+                </footer >
         </>
     )
 }
